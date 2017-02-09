1fff4d2ca344ec35d6d56be07ea135870b2dae29
jakub_desktop <pay64k@gmail.com>
2017-02-09 21:18:33 +0100

    initial

---
+++ ar-sandbox-unity/Assets/KinectDepth.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class KinectDepth : MonoBehaviour {
+    private KinectSensor _Sensor;
+    private ushort[] _DepthData;
+
+    public GameObject _DepthManagerGameObject;
+
+    private DepthFrame DFrame;
+    private DepthFrameReader _Reader;
+
+    private Gradient gradient;
+
+    private DepthSourceManager _Manager;
+
+    private int imageWidth, imageHeight;
+
+    private Color minColor = Color.green;
+    private Color maxColor = Color.red;
+    private Color lerpedColor = Color.white;
+
+    void Start () {
+
+        _Sensor = KinectSensor.GetDefault();
+        _Manager = _DepthManagerGameObject.GetComponent<DepthSourceManager>();
+        imageWidth = _Sensor.DepthFrameSource.FrameDescription.Width;
+        imageHeight = _Sensor.DepthFrameSource.FrameDescription.Height;
+
+
+        //_Reader = _Sensor.DepthFrameSource.OpenReader();
+        //print(_Reader.DepthFrameSource.FrameDescription.BytesPerPixel);
+        //print(_Reader.DepthFrameSource.FrameDescription.DiagonalFieldOfView);
+        //print(_Reader.DepthFrameSource.FrameDescription.HorizontalFieldOfView);
+        //print(_Reader.DepthFrameSource.FrameDescription.VerticalFieldOfView);
+        //print(_Reader.DepthFrameSource.FrameDescription.Height);
+        //print(_Reader.DepthFrameSource.FrameDescription.Width);
+
+        if (!_Sensor.IsOpen)
+        {
+            _Sensor.Open();
+            print("----Kinect started----");
+        }
+    }
+	
+	void Update () {
+        _DepthData = _Manager.GetData();
+        print(_DepthData.Length);
+        lerpedColor = Color.Lerp(minColor, maxColor, );
+
+    }
+}
+
