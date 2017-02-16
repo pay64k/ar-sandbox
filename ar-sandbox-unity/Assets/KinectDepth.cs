cde6dcd2c93c492eefcbc0bb3ee21e7e571677ae
jakub_desktop <pay64k@gmail.com>
2017-02-16 18:47:48 +0100

    works, but bad quality

--- ar-sandbox-unity/Assets/KinectDepth.cs
+++ ar-sandbox-unity/Assets/KinectDepth.cs
@@ -1,20 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Windows.Kinect;
 
 public class KinectDepth : MonoBehaviour {
     private KinectSensor _Sensor;
-    private ushort[] _DepthData;
 
-    public GameObject _DepthManagerGameObject;
-
-    private DepthFrame DFrame;
     private DepthFrameReader _Reader;
 
     private Gradient gradient;
-
-    private DepthSourceManager _Manager;
 
     private int imageWidth, imageHeight;
 
@@ -22,34 +17,113 @@ public class KinectDepth : MonoBehaviour {
     private Color maxColor = Color.red;
     private Color lerpedColor = Color.white;
 
-    void Start () {
+    private int minDist = 1000;
+    private int maxDist = 2000;
 
+    private ushort[] _Data;
+    private byte[] pixels;
+
+    Texture2D texture;
+
+    void Start () {
+        texture = new Texture2D(512, 424, TextureFormat.RGBA32, false);
         _Sensor = KinectSensor.GetDefault();
-        _Manager = _DepthManagerGameObject.GetComponent<DepthSourceManager>();
+
+        pixels = new byte[512*424*4];
+
         imageWidth = _Sensor.DepthFrameSource.FrameDescription.Width;
         imageHeight = _Sensor.DepthFrameSource.FrameDescription.Height;
 
+        //ushort minDepth = _Sensor.DepthFrameSource.DepthMinReliableDistance;
+        //ushort maxDepth = _Sensor.DepthFrameSource.DepthMinReliableDistance;
+        //print(minDepth.ToString() + " " + maxDepth.ToString());
 
-        //_Reader = _Sensor.DepthFrameSource.OpenReader();
-        //print(_Reader.DepthFrameSource.FrameDescription.BytesPerPixel);
-        //print(_Reader.DepthFrameSource.FrameDescription.DiagonalFieldOfView);
-        //print(_Reader.DepthFrameSource.FrameDescription.HorizontalFieldOfView);
-        //print(_Reader.DepthFrameSource.FrameDescription.VerticalFieldOfView);
-        //print(_Reader.DepthFrameSource.FrameDescription.Height);
-        //print(_Reader.DepthFrameSource.FrameDescription.Width);
 
         if (!_Sensor.IsOpen)
         {
             _Sensor.Open();
+            _Reader = _Sensor.DepthFrameSource.OpenReader();
+            _Data = new ushort[_Sensor.DepthFrameSource.FrameDescription.LengthInPixels];
             print("----Kinect started----");
         }
     }
 	
 	void Update () {
-        _DepthData = _Manager.GetData();
-        print(_DepthData.Length);
-        lerpedColor = Color.Lerp(minColor, maxColor, );
+        Renderer renderer = GetComponent<Renderer>();
+        var frame = _Reader.AcquireLatestFrame();
 
+        if (frame != null)
+        {
+            frame.CopyFrameDataToArray(_Data);
+            frame.Dispose();
+            frame = null;
+
+            
+            for (int colorIndex = 0; colorIndex < 512*424*4; colorIndex+=4)
+                {
+
+                int depth = _Data[colorIndex / 4];
+
+                byte[] bla = StringToByteArray(ColorToHex(lerpedColor)); 
+
+                if(depth >= minDist && depth <= maxDist )
+                {
+                    bla = StringToByteArray(ColorToHex(minColor));
+                }
+                if(depth >= maxDist)
+                {
+                   bla=  StringToByteArray(ColorToHex(maxColor));
+                }
+
+                pixels[colorIndex] = bla[0];
+                pixels[colorIndex + 1] = bla[1];
+                pixels[colorIndex + 2] = bla[2];
+                pixels[colorIndex + 3] = 0x00;
+
+            }
+                
+            //lerpedColor = Color.Lerp(minColor, maxColor, );
+
+            texture.LoadRawTextureData(pixels);
+            texture.Apply();
+            renderer.material.mainTexture = texture;
+        }
+        //print("Data len:" + _Data.Length);
+        //print(pixels.Length/4);
+        
+    }
+
+    string ColorToHex(Color32 color)
+    {
+        string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+        return hex;
+    }
+
+    Color HexToColor(string hex)
+    {
+        byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+        byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+        byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+        return new Color32(r, g, b, 255);
+    }
+
+    public static byte[] StringToByteArray(string hex)
+    {
+        int NumberChars = hex.Length;
+        byte[] bytes = new byte[NumberChars / 2];
+        for (int i = 0; i < NumberChars; i += 2)
+            bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+        return bytes;
+    }
+
+    void PrintFrameSpecs()
+    {
+        //print(_Reader.DepthFrameSource.FrameDescription.BytesPerPixel);         //2
+        //print(_Reader.DepthFrameSource.FrameDescription.DiagonalFieldOfView);   //89.5
+        //print(_Reader.DepthFrameSource.FrameDescription.HorizontalFieldOfView); //70.6
+        //print(_Reader.DepthFrameSource.FrameDescription.VerticalFieldOfView);   //60
+        //print(_Reader.DepthFrameSource.FrameDescription.Height);    //424
+        //print(_Reader.DepthFrameSource.FrameDescription.Width);     //512
     }
 }
 
