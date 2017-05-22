bdef69134b269043fe161c89aa9368ede06cf3f8
pay64k <pay64k@gmail.com>
2017-05-22 09:46:39 +0200

    update missing files

---
+++ ar-sandbox-unity/Assets/KinectView/Scripts/FileSaver.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileSaver {
+
+    private int fileIndexCounter = 0;
+
+    public void SaveDepthMap(ushort[] map)
+    {
+        MonoBehaviour.print("written file no: " + fileIndexCounter.ToString());
+        string[] foo = map.OfType<object>().Select(o => o.ToString()).ToArray();
+        System.IO.File.WriteAllLines(@"C:\Users\s127578\Desktop\testFolder\HeightMap" + fileIndexCounter + ".txt", foo);
+        fileIndexCounter++;
+    }
+
+    public void SaveCalibration(Vector3[] list)
+    {
+        //string[] foo = list.OfType<object>().Select(o => o.ToString()).ToArray();
+
+        string[] foo = Vector3ToString(list);
+
+        System.IO.File.WriteAllLines(@"C:\Users\s127578\Desktop\testFolder\positions", foo);
+        foreach(string item in foo)
+        {
+            MonoBehaviour.print(item);
+        }
+    }
+
+    public Vector3[] ReadCalibration()
+    {
+        string[] lines = System.IO.File.ReadAllLines(@"C:\Users\s127578\Desktop\testFolder\positions");
+        Vector3[] positions = new Vector3[4];
+        for(int i = 0; i < 4; i++)
+        {
+            positions[i] = StringToVector3(lines[i]);
+        }
+
+        return positions;
+    }
+
+    public static Vector3 StringToVector3(string sVector)
+    {
+        // Remove the parentheses
+        if (sVector.StartsWith("(") && sVector.EndsWith(")"))
+        {
+            sVector = sVector.Substring(1, sVector.Length - 2);
+        }
+
+        // split the items
+        string[] sArray = sVector.Split(',');
+
+        // store as a Vector3
+        Vector3 result = new Vector3(
+            float.Parse(sArray[0]),
+            float.Parse(sArray[1]),
+            float.Parse(sArray[2]));
+
+        return result;
+    }
+
+    public string[] Vector3ToString(Vector3[] list)
+    {
+        string[] pos = new string[4];
+        for (int i = 0; i < 4; i++)
+        {
+            pos[i] = list[i].ToString("G4");
+        }
+        return pos;
+    }
+}
---
+++ ar-sandbox-unity/Assets/Shaders/Example.shader
@@ -0,0 +1,85 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Unlit/AlessandroExample"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		//HeightMapFloat("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float4 color : COLOR0;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D HeightMap;
+
+			fixed4 get_color(float height)
+			{
+				fixed4 bla = (1.0, 0.0, 0.0, 1.0);
+
+				if (height < 200.0) {
+					bla = fixed4(0.0, 1.0, 0.0, 1.0);
+				}
+				else {
+					bla = fixed4(0.0, 0.0, 1.0, 1.0);
+				}
+				return bla;
+			}
+			
+			v2f vert(appdata v)
+			{
+				v2f o;
+				//float height = v.vertex.z + 1.0;// 
+				float4 vertexws = mul(unity_ObjectToWorld, v.vertex);
+
+				float height = vertexws.y + tex2Dlod(_MainTex, float4(v.uv, 0, 0)).x;
+
+				vertexws = float4(vertexws.x, height, vertexws.z , vertexws.w);
+				
+				o.color = get_color(height);
+
+				o.vertex = mul(UNITY_MATRIX_VP, vertexws);
+
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = i.color;
+			/*	fixed4 col = fixed4(i.uv, 0, 1);*/
+				return col;
+				//return half4(1.0, 0.0, 0.0, 1.0);
+			}
+			ENDCG
+		}
+	}
+}
